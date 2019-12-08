7d55ada4c78ccc1ebec251974c031456098aa376
ARAB <ahmedmohamed23345@gmail.com>
2019-12-08 16:00:16 +0200

    ch

--- app/src/main/java/com/creativeshare/end_point/activities_fragments/activity_fragments/activity_Qr/ScanActivity.java
+++ app/src/main/java/com/creativeshare/end_point/activities_fragments/activity_fragments/activity_Qr/ScanActivity.java
@@ -44,12 +44,13 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class ScanActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler{
+public class ScanActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
     private ActivityScanBinding binding;
     private final int CAMERA_REQ = 1022;
     private final String camera_perm = Manifest.permission.CAMERA;
-private Preferences preferences;
-private UserModel userModel;
+    private Preferences preferences;
+    private UserModel userModel;
+
     @Override
     protected void attachBaseContext(Context newBase) {
         Paper.init(newBase);
@@ -62,11 +63,11 @@ private UserModel userModel;
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_scan);
         initView();
-checkCameraPermission();
+        checkCameraPermission();
 
     }
-    private void checkCameraPermission()
-    {
+
+    private void checkCameraPermission() {
         if (ContextCompat.checkSelfPermission(this, camera_perm) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{camera_perm}, CAMERA_REQ);
 
@@ -76,14 +77,15 @@ checkCameraPermission();
 
         }
     }
+
     @SuppressLint("RestrictedApi")
     private void initView() {
         binding.scannerView.setFormats(ZXingScannerView.ALL_FORMATS);
         binding.scannerView.setResultHandler(this);
         binding.scannerView.setAutoFocus(false);
-preferences=Preferences.getInstance();
-userModel=preferences.getUserData(this);
-         }
+        preferences = Preferences.getInstance();
+        userModel = preferences.getUserData(this);
+    }
 
 
     @Override
@@ -96,40 +98,41 @@ userModel=preferences.getUserData(this);
 
         Date currentLocalTime = cal.getTime();
         DateFormat date = new SimpleDateFormat("HH:mm", Locale.ENGLISH);
-        String str_date="08:00";
-        String end_date="12:00";
-        Date start = null,end = null,cureenttime = null;
+        String str_date = "08:00";
+        String end_date = "12:00";
+        Date start = null, end = null, cureenttime = null;
 
         String localTime = date.format(currentLocalTime);
 
         try {
-           start=date.parse(str_date);
-            end=date.parse(end_date);
-            cureenttime=date.parse(localTime);
+            start = date.parse(str_date);
+            end = date.parse(end_date);
+            cureenttime = date.parse(localTime);
         } catch (ParseException e) {
-Log.e("err",e.getMessage()+localTime);        }
-cal.setTime(cureenttime);
+            Log.e("err", e.getMessage() + localTime);
+        }
+        cal.setTime(cureenttime);
         cal2.setTime(start);
         cal3.setTime(end);
         int startSHhour = cal2.get(Calendar.HOUR_OF_DAY);
         int startSHmin = cal2.get(Calendar.MINUTE);
-        int timeStart = startSHhour*60 + startSHmin;
+        int timeStart = startSHhour * 60 + startSHmin;
         int nowHour = cal.get(Calendar.HOUR_OF_DAY);
         int nowMin = cal.get(Calendar.MINUTE);
-        int timeNow = nowHour*60 + nowMin;
+        int timeNow = nowHour * 60 + nowMin;
         int stopSHhour = cal3.get(Calendar.HOUR_OF_DAY);
         int stopSHmin = cal3.get(Calendar.MINUTE);
-        int timeStop = stopSHhour*60 + stopSHmin;
+        int timeStop = stopSHhour * 60 + stopSHmin;
 // you can get seconds by adding  "...:ss" to itظ*
-Log.e("time",timeStart+" "+timeStop+" "+timeNow+" "+result.getText()+" "+localTime);
-if(timeStop>timeNow&&timeStart<=timeNow){
-    scanopen(result.getText(),localTime);
-}
-else if(timeStop<=timeNow) {
-    scanend(result.getText(),localTime);
-}
+        Log.e("time", timeStart + " " + timeStop + " " + timeNow + " " + result.getText() + " " + localTime);
+        if (timeStop > timeNow && timeStart <= timeNow) {
+            scanopen(result.getText(), localTime);
+        } else if (timeStop <= timeNow) {
+            scanend(result.getText(), localTime);
+        }
 
     }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -145,60 +148,53 @@ else if(timeStop<=timeNow) {
     }
 
     @Override
-    protected void onResume()
-    {
+    protected void onResume() {
         super.onResume();
         checkCameraPermission();
     }
+
     @Override
-    protected void onStop()
-    {
+    protected void onStop() {
         super.onStop();
         if (binding.scannerView != null) {
             binding.scannerView.stopCamera();
         }
     }
-    private void scanopen(String code, String localTime)
-    {
-        final ProgressDialog dialog = Common.createProgressDialog(this,getString(R.string.wait));
+
+    private void scanopen(String code, String localTime) {
+        final ProgressDialog dialog = Common.createProgressDialog(this, getString(R.string.wait));
         dialog.setCancelable(false);
         dialog.show();
-
-
-
 
 
         try {
 
             Api.getService(Tags.base_url)
-                    .scanopen(userModel.getId()+"",code,localTime)
+                    .scanopen(userModel.getId() + "", code, localTime)
                     .enqueue(new Callback<ScanResultModel>() {
                         @Override
                         public void onResponse(Call<ScanResultModel> call, Response<ScanResultModel> response) {
                             dialog.dismiss();
-                            if (response.isSuccessful()&&response.body()!=null)
-                            {
-                                Toast.makeText(ScanActivity.this,"start"+response.body().getAttendance_time(),Toast.LENGTH_LONG).show();
-                           NavigatetToTimesActivity();
+                            if (response.isSuccessful() && response.body() != null) {
+                                Toast.makeText(ScanActivity.this, "start" + response.body().getAttendance_time(), Toast.LENGTH_LONG).show();
+                                NavigatetToTimesActivity();
 
-                            }else
-                            {
+                            } else {
                                 if (response.code() == 500) {
                                     Toast.makeText(ScanActivity.this, "Server Error", Toast.LENGTH_SHORT).show();
                                     try {
-                                        Log.e("error",response.code()+"_"+response.errorBody().string());
+                                        Log.e("error", response.code() + "_" + response.errorBody().string());
                                     } catch (IOException e) {
                                         e.printStackTrace();
                                     }
 
 
-                                }else
-                                {
+                                } else {
                                     Toast.makeText(ScanActivity.this, getString(R.string.failed), Toast.LENGTH_SHORT).show();
 
                                     try {
 
-                                        Log.e("error",response.code()+"_"+response.errorBody().string());
+                                        Log.e("error", response.code() + "_" + response.errorBody().string());
                                     } catch (IOException e) {
                                         e.printStackTrace();
                                     }
@@ -210,72 +206,63 @@ else if(timeStop<=timeNow) {
                         public void onFailure(Call<ScanResultModel> call, Throwable t) {
                             try {
                                 dialog.dismiss();
-                                if (t.getMessage()!=null)
-                                {
-                                    Log.e("error",t.getMessage());
-                                    if (t.getMessage().toLowerCase().contains("failed to connect")||t.getMessage().toLowerCase().contains("unable to resolve host"))
-                                    {
-                                        Toast.makeText(ScanActivity.this,R.string.something, Toast.LENGTH_SHORT).show();
-                                    }else
-                                    {
-                                        Toast.makeText(ScanActivity.this,t.getMessage(), Toast.LENGTH_SHORT).show();
+                                if (t.getMessage() != null) {
+                                    Log.e("error", t.getMessage());
+                                    if (t.getMessage().toLowerCase().contains("failed to connect") || t.getMessage().toLowerCase().contains("unable to resolve host")) {
+                                        Toast.makeText(ScanActivity.this, R.string.something, Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        Toast.makeText(ScanActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
                                     }
                                 }
 
-                            }catch (Exception e){}
+                            } catch (Exception e) {
+                            }
                         }
                     });
-        }catch (Exception e){
+        } catch (Exception e) {
             dialog.dismiss();
 
         }
     }
 
     private void NavigatetToTimesActivity() {
-        preferences.create_update_time(ScanActivity.this,"1");
+        preferences.create_update_time(ScanActivity.this, "1");
 
         Intent intent = new Intent(ScanActivity.this, MyTimesActivity.class);
-        startActivityForResult(intent,3);
+        startActivityForResult(intent, 3);
         finish();
     }
 
-    private void scanend(String code, String localTime)
-    {
-        final ProgressDialog dialog = Common.createProgressDialog(this,getString(R.string.wait));
+    private void scanend(String code, String localTime) {
+        final ProgressDialog dialog = Common.createProgressDialog(this, getString(R.string.wait));
         dialog.setCancelable(false);
         dialog.show();
-
-
-
 
 
         try {
 
             Api.getService(Tags.base_url)
-                    .scanend(userModel.getId()+"",code,localTime)
+                    .scanend(userModel.getId() + "", code, localTime)
                     .enqueue(new Callback<ScanResultModel>() {
                         @Override
                         public void onResponse(Call<ScanResultModel> call, Response<ScanResultModel> response) {
                             dialog.dismiss();
-                            if (response.isSuccessful()&&response.body()!=null)
-                            {
-                                Toast.makeText(ScanActivity.this,"End"+response.body().getDeparture_time(),Toast.LENGTH_LONG).show();
+                            if (response.isSuccessful() && response.body() != null) {
+                                Toast.makeText(ScanActivity.this, "End" + response.body().getDeparture_time(), Toast.LENGTH_LONG).show();
 
                                 NavigatetToTimesActivity();
 
-                            }else
-                            {
+                            } else {
                                 if (response.code() == 500) {
                                     Toast.makeText(ScanActivity.this, "Server Error", Toast.LENGTH_SHORT).show();
 
 
-                                }else
-                                {
+                                } else {
                                     Toast.makeText(ScanActivity.this, getString(R.string.failed), Toast.LENGTH_SHORT).show();
 
                                     try {
 
-                                        Log.e("error",response.code()+"_"+response.errorBody().string());
+                                        Log.e("error", response.code() + "_" + response.errorBody().string());
                                     } catch (IOException e) {
                                         e.printStackTrace();
                                     }
@@ -287,22 +274,20 @@ else if(timeStop<=timeNow) {
                         public void onFailure(Call<ScanResultModel> call, Throwable t) {
                             try {
                                 dialog.dismiss();
-                                if (t.getMessage()!=null)
-                                {
-                                    Log.e("error",t.getMessage());
-                                    if (t.getMessage().toLowerCase().contains("failed to connect")||t.getMessage().toLowerCase().contains("unable to resolve host"))
-                                    {
-                                        Toast.makeText(ScanActivity.this,R.string.something, Toast.LENGTH_SHORT).show();
-                                    }else
-                                    {
-                                        Toast.makeText(ScanActivity.this,t.getMessage(), Toast.LENGTH_SHORT).show();
+                                if (t.getMessage() != null) {
+                                    Log.e("error", t.getMessage());
+                                    if (t.getMessage().toLowerCase().contains("failed to connect") || t.getMessage().toLowerCase().contains("unable to resolve host")) {
+                                        Toast.makeText(ScanActivity.this, R.string.something, Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        Toast.makeText(ScanActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
                                     }
                                 }
 
-                            }catch (Exception e){}
+                            } catch (Exception e) {
+                            }
                         }
                     });
-        }catch (Exception e){
+        } catch (Exception e) {
             dialog.dismiss();
 
         }
